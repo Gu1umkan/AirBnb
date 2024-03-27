39f141fcc707bee84872f51f083c12254cff187d
nurtaazim <nurtaazimmukanov24@gmail.com>
2024-03-27 15:40:20 +0600

    commmmit

--- src/main/java/airbnb/api/BookingApi.java
+++ src/main/java/airbnb/api/BookingApi.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/booking")
-public class BookingAPI {
+public class BookingApi {
      private final BookingService bookingService;
      @Secured("USER")
      @PostMapping("/save/{announcementId}")
